3f3a916a8796595e7f6af998f57d9164a21e78c1
AndyK111 <AndyKempf@u.boisestate.edu>
2025-10-01 14:54:39 -0600

    Finished top.v (untested)

--- top.v
+++ top.v
@@ -1,10 +1,15 @@
 // Implement top level module
 module top(
-    input [3:0] sw
-    output [2:0] led
+    input [7:0] sw,
+    output [4:0] led
 );
 
 light l (sw[0], sw[1], led[0]);
 
 adder a0 (sw[2], sw[3], led[1], led[2]);
+
+wire carry_out_fa0;
+full_adder fa0 (sw[5], sw[4], 0, led[3], carry_out_fa0);
+full_adder fa1 (sw[7], sw[6], carry_out_fa0, led[4]);
+assign led[5] = carry_out_fa0;
 
