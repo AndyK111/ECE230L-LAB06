0ea91380a652a968a577dd34200656167fd74976
AndyK111 <AndyKempf@u.boisestate.edu>
2025-09-30 23:25:18 -0600

    implemented (untested) light.v

--- light.v
+++ light.v
@@ -1,8 +1,11 @@
 module light(
     // Declare downstairs and upstairs input
+    input downstairs, upstairs
     // Declare stair light output
+    output light
 );
 
     // Enter logic equation here
+    assign light = downstairs ^ upstairs;
 
 endmodule
