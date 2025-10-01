c064ba723061c14cf6a823bb9d9e1a64048038a6
AndyK111 <AndyKempf@u.boisestate.edu>
2025-10-01 14:59:47 -0600

    Added constant 0 to top.v fa0

--- top.v
+++ top.v
@@ -9,7 +9,6 @@ light l (sw[0], sw[1], led[0]);
 adder a0 (sw[2], sw[3], led[1], led[2]);
 
 wire carry_out_fa0;
-full_adder fa0 (sw[5], sw[4], 0, led[3], carry_out_fa0);
+full_adder fa0 (sw[5], sw[4], 1'b0, led[3], carry_out_fa0); //1'b0 is a constant 0, this does not input a carry_in, but I am still required to use a full_adder
 full_adder fa1 (sw[7], sw[6], carry_out_fa0, led[4]);
 assign led[5] = carry_out_fa0;
-
